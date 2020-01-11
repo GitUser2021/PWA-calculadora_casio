77688fa4c91515e215ccfe24deddef31b2b768ec
GitUser2021 <58796929+GitUser2021@users.noreply.github.com>
2020-01-11 04:06:16 -0300

    reset soft

--- js/memory.js
+++ js/memory.js
@@ -13,6 +13,15 @@ function Memory() {
         this.operation = ''
         this.status_a = true
         this.status_b = false
+        aux_display.data_a = ''
+        aux_display.value = '' // limpio la memoria del display auxiliar.
+        controlOperations.prev_number = null
+        controlOperations.prev_operator = null
+    }
+    this.reset.mem_b = () => {
+        memory.value_b = 0
+        memory.status_a = true
+        memory.status_b = false
     }
 }
 
@@ -32,7 +41,7 @@ function controlNumbers(value) {
     } else {
         if (memory.operation == '=') {
             memory.operation = ''
-            controlOperations_clear() // reset de variables temporales.
+           // controlOperations_clear() // reset de variables temporales.
             memory.value_a = 0
             memory.value_a += value
 
@@ -51,70 +60,69 @@ function controlNumbers(value) {
 
 // controlador de operaciones.
 function controlOperations(value) {
-    if (value == 'AC' || value == 'C') { // filtro para no poner en memory.operation ni 'AC' o 'C' ya que daria problemas.
-        switch (value) {
-            case 'AC':
-                aux_display.show('AC')
-                display.clear() // limpio el display.
-                memory.reset() // reseteo la memoria al estado original.
-                controlOperations_clear() // reset de variables temporales.
-                break
+    switch (value) { // filtro para no poner en memory.operation ni 'AC' o 'C' ya que daria problemas.
+        case 'AC':
+            aux_display.show('AC')
+            display.clear() // limpio el display.
+            memory.reset() // reseteo la memoria al estado original.
+            return // salgo de la funcion.
 
-            case 'C':
-                display.clear()
-                if (memory.status_b && memory.value_b != 0) {
-                    memory.value_b = 0
-                    memory.status_a = true
-                    memory.status_b = false
-                    aux_display.show(aux_display.data_a)
-                } else {
-                    aux_display.show('C')
-                    memory.value_a = 0
-                    aux_display.value = '' // limpio la memoria del display auxiliar.
-                    controlOperations_clear() // reset de variables temporales.
-                }
-                break
-        }
-    } else {
-        if (memory.status_b) {
-            resolve() // resuelve las operaciones.
-            memory.status_b = false
-            memory.status_a = true
-            memory.value_b = 0
-            memory.operation = value
-            controlOperations_clear() // reset de variables temporales.
-        } else {
-            if (value == '=') {
-                if (memory.value_a == 0 && memory.value_b == 0 && memory.operation == '=') {
-                    controlOperations_clear() // reset de variables temporales.
-                }
-                if (controlOperations.prev_number == null) { // variable temporal numero previo.
-                    controlOperations.prev_number = memory.value_a
-                }
-                if (controlOperations.prev_operator == null) { // variable temporal operacion previa.
-                    controlOperations.prev_operator = memory.operation
-                }
-                memory.value_b = controlOperations.prev_number
-                memory.operation = controlOperations.prev_operator
-                resolve() // resuelve las operaciones.
-                memory.status_b = false
-                memory.status_a = true
-                memory.value_b = 0
-                memory.operation = value
+        case 'C':
+            display.clear()
+            if (memory.status_b) { 
+                memory.reset.mem_b() // solo borro la memory_value_b.
+                aux_display.show(aux_display.data_a)
+                return // salgo de la funcion.
             } else {
-                memory.operation = value
-                controlOperations_clear() // reset de variables temporales.
+                aux_display.show('C')
+                memory.reset() // reseteo la memoria al estado original.
+                return // salgo de la funcion.
             }
-        }
-        render() // mostrar datos en el display.
+
+        case '%':
+            controlOperations.prev_operator = memory.operation // guardo el operador previo al %.
+            memory.operation = value
+            resolve()
+            memory.operation = controlOperations.prev_operator
+            value = '='
+            break
     }
+    if (memory.status_b) {
+        resolve() // resuelve las operaciones.
+        memory.operation = value
+        memory.reset.mem_b() // solo borro la memory_value_b.
+        //  controlOperations_clear() // reset de variables temporales.
+    } else {
+        if (value == '=') {
+            if (memory.value_a == 0 && memory.value_b == 0 && memory.operation == '=') {
+                // controlOperations_clear() // reset de variables temporales.
+            }
+            if (controlOperations.prev_number == null) { // variable temporal numero previo.
+                controlOperations.prev_number = memory.value_a
+            }
+            if (controlOperations.prev_operator == null) { // variable temporal operacion previa.
+                controlOperations.prev_operator = memory.operation
+            }
+            memory.value_b = controlOperations.prev_number
+            memory.operation = controlOperations.prev_operator
+            resolve() // resuelve las operaciones.
+            memory.operation = value
+            memory.reset.mem_b() // solo borro la memory_value_b.
+        } else {
+            memory.operation = value
+            // controlOperations_clear() // reset de variables temporales.
+        }
+    }
+
+    render() // mostrar datos en el display.  
+    aux_display.data_b = ''
 }
 
-// reset de variables temporales.
-function controlOperations_clear() {
-    controlOperations.prev_number = null
-    controlOperations.prev_operator = null
-}
+// reset de variables temporales. //************************* POSIBLE ELIMINACION */
+// function controlOperations_clear() {
+//     controlOperations.prev_number = null
+//     controlOperations.prev_operator = null
+// }
 
 // resuelve las operaciones.
 function resolve() {
@@ -139,8 +147,35 @@ function resolve() {
             fix_decimal(MAX_DECIMALES)
             break
 
+        case '%':
+            memory.value_a = parseFloat(memory.value_a)
+            if (memory.status_a) {
+                memory.value_a = parseFloat(memory.value_a) / 100
+            } else {
+                if (controlOperations.prev_operator == '+' || controlOperations.prev_operator == '-') {
+                    memory.value_b = memory.value_a * (parseFloat(memory.value_b) / 100)
+                } else {
+                    memory.value_b = (parseFloat(memory.value_b) / 100)
+                }
+            }
+            // actualizo con el valor resuelto para poder mostrar en el display
+            // ej: 10 + 50%
+            // se muestra como 10 + 5(valor resuelto) = 15
+            // en vez de 10 + 50% = 15.
+            aux_display.data_b = memory.value_b
+            fix_decimal(MAX_DECIMALES)
+            break
+
         default:
             break
+    }
+    // mensaje si el resultado es infinito.
+    if (memory.value_a == 'Infinity') {
+        ref_aux_display.innerHTML = ''
+        ref_display.innerHTML = '<span id = "error">Numero infinito!</span>'
+        memory.reset()
+        aux_display.data_a = ''
+        throw Error('Numero infinito!')
     }
 }
 
